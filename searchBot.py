b6b734567e5737a3e5613857f00119f568a55a34
Praneetha Kishore Kumar <82694786+Pranee1999@users.noreply.github.com>
2021-04-22 10:41:14 +0530

    Update searchBot.py

--- searchBot.py
+++ searchBot.py
@@ -1,10 +1,10 @@
 import tweepy
 import time
 
-consumer_key="stsY7lMpccp39pCQaxfiakphx"
-consumer_secret="kFIFF8BcdnJFu4jNv5DDoOXQyKxVRBTVT9rbhjB3s8LxWY7YQN"
-key="1354332471702110208-MfH9dQdUOvjXtCZ9jwbKDOSC7hVoVk"
-secret="uWaRLYqPCm1qjneTjUUQoogd3O5Hz7Aoob9gacwr7wltL"
+# Put in the consumer_key
+# Put in the consumer_secret
+# Put in the key
+# Put in the secret
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
